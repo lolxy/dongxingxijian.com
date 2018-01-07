d6e538d832d23df0e9da2458b9d0ce1e5b22735e
lolxy <lolxy@users.noreply.github.com>
2018-01-07 21:09:15 +0800

    前台全部静态页面更新
    前台全部静态页面更新

--- 静态页面/js/common.js
+++ 静态页面/js/common.js
@@ -9,3 +9,28 @@ $(".qvxiao").on("click",function(){
   $(this).hide();
   $(".search-wrap").removeClass("active");
 });
+
+// 侧边栏菜单切换
+$(".menu").click(function(){
+  if($(this).hasClass("open")){
+    $(this).removeClass("open");
+    $("#slide-menu").css({"right":"-45%"});
+    $("#header").css({"right":"0"});
+  }else{
+    $(this).addClass("open");
+    $("#slide-menu").css({"right":"0"});
+    $("#header").css({"right":"45%"});
+  }
+});
+
+$(document).click(function(){
+  if($(".menu").hasClass("open")){
+    $(".menu").removeClass("open");
+    $("#slide-menu").css({"right":"-45%"});
+    $("#header").css({"right":"0"});
+  }
+});
+
+$("#slide-menu,.menu").click(function(e){
+  e.stopPropagation();
+});
